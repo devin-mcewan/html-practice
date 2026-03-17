f26982b0aa2b91da0aa7f051c5c43c83c07ebc8a
Devin <devster99@gmail.com>
2026-03-16 21:48:31 -0600

    added a new function that allows passing parameters. Confirmed it was working

--- scripts/script.js
+++ scripts/script.js
@@ -5,3 +5,11 @@ function introduce() {
   console.log(`Hello, my name is ${name} and my goal is to ${goal}.`);
 }
 introduce();
+
+function introduceWithHobby(hobby) {
+  console.log(
+    `Hello, my name is ${name}, my goal is to ${goal}, and I enjoy ${hobby}.`,
+  );
+}
+
+introduceWithHobby("racing cars");
